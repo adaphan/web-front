769cd1cd6d8ea2e75819bebbecef5b3150605758
syaikhanagil <syaikhanagil.id@gmail.com>
2021-06-14 03:21:01 +0700

    Deploy meta FB

--- index.prod.js
+++ index.prod.js
@@ -44,6 +44,7 @@ app.get('/*', (req, res) => {
 <meta property="og:url" content="https://adaphan.com">
 <meta property="og:title" content="Adaphan Exchanger">
 <meta property="og:description" content="Adaphan Penukaran Digital - Partner Resmi Jual Beli Voucher Indodax">
+<meta name="facebook-domain-verification" content="3gs0h8v2109xsxjco4w6ka1lvi5agb" />
 <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
 <!-- Google Tag Manager -->
